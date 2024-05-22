06c1670efd4c94ae2c5e034be6ef936380fb78d5
iOmega8561 <giusepperocco38@gmail.com>
2024-05-22 19:47:13 +0200

    Moved query methods to manager class

--- src/detergents/dao/Manager.java
+++ src/detergents/dao/Manager.java
@@ -2,11 +2,14 @@ package detergents.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public final class Manager {
 
     // Singleton
+    
     private static Manager INSTANCE;
 
     public static Manager getInstance() {
@@ -17,9 +20,11 @@ public final class Manager {
     }
 
     // Init
+    
     private Manager() {}
 
     // Properties
+
     private String url = "jdbc:mysql://localhost:3306/detergents?serverTimezone=UTC";
 
     private Connection connection;
@@ -32,6 +37,22 @@ public final class Manager {
         }
 
         return connection;
+    }
+
+    public ResultSet query(String queryStatement) throws SQLException {
+        Connection conn = this.getConnection();
+
+        PreparedStatement preparedStatement = conn.prepareStatement(queryStatement); 
+        ResultSet result = preparedStatement.executeQuery();
+
+        return result;
+    }
+
+    public void queryVoid(String queryStatement) throws SQLException {
+        Connection conn = this.getConnection();
+
+        PreparedStatement preparedStatement = conn.prepareStatement(queryStatement); 
+        preparedStatement.execute();
     }
 
 }
