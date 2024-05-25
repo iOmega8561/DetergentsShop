fc69feb9ef0985ab718b9af9c2cd6a8b3cc8e478
zRedk <federica.mosca2001@gmail.com>
2024-05-25 12:43:46 +0200

    created entity prodotto

---
+++ src/main/com/romomo/entity/Prodotto.java
@@ -0,0 +1,76 @@
+/*
+ Copyright (c) 2024 Giuseppe Rocco, Federica Mosca, Vittorio Monfrecola
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+*/
+
+package com.romomo.entity;
+
+public class Prodotto {
+
+    private String codice;
+
+    private String nome;
+
+    private String descrizione;
+
+    private float prezzo;
+
+    private int quantita;
+
+    public String getCodice() {
+        return codice;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public float getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(float prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public int getQuantita() {
+        return quantita;
+    }
+
+    public void setQuantita(int quantita) {
+        this.quantita = quantita;
+    }
+
+    public Prodotto(
+        String codice, 
+        String nome, 
+        String descrizione, 
+        float prezzo,
+        int quantita
+    ) {
+        this.codice = codice;
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.quantita = quantita;
+    }
+
+}
