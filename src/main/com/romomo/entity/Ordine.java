36bd164205d35e2e2292386c2041a71e552bc4a0
Vittorio Monfrecola <vittoriomonfrecola24@gmail.com>
2024-05-25 15:30:17 +0200

    created entity Ordine

---
+++ src/main/com/romomo/entity/Ordine.java
@@ -0,0 +1,27 @@
+ /*
+ Copyright (c) 2024 Giuseppe Rocco, Federica Mosca, Vittorio Monfrecola
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ */
+
+package com.romomo.entity;
+
+public class Ordine {
+	
+	private String numeroOrdini;
+	
+	public String getnumeroOrdini() {
+        return numeroOrdini;
+    }
+	
+	public numeroOrdini(
+	        String numeroOrdini
+	    ) {
+	        this.numeroOrdini = numeroOrdini;
+	    }
+}
