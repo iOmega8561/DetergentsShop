3cb8f45b3c2425b89eb8ac91938b2dda895db967
Vittorio Monfrecola <vi.monfrecola@studenti.unina.it>
2024-05-25 17:53:18 +0200

    Created entity Ordine Created DAO OrdineDao

---
+++ src/main/com/romomo/entity/Ordine.java
@@ -0,0 +1,17 @@
+package com.romomo.entity;
+
+public class Ordine {
+	
+	private String numeroOrdini;
+	
+	public String getNumeroOrdini() {
+        return numeroOrdini;
+    }
+	
+    public Ordine(String numeroOrdini) {
+        this.numeroOrdini = numeroOrdini;
+    }
+
+}
+
+
