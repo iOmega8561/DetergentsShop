73d05d4f8c5d465b5b1673599839e3e402d587b1
Vittorio Monfrecola <vi.monfrecola@studenti.unina.it>
2024-05-27 16:02:33 +0200

    Completed entity Ordine

--- src/main/com/romomo/entity/Ordine.java
+++ src/main/com/romomo/entity/Ordine.java
@@ -1,15 +1,72 @@
 package com.romomo.entity;
 
+import java.time.LocalDate;
+
 public class Ordine {
 	
-	private String numeroOrdini;
-	
-	public String getNumeroOrdini() {
-        return numeroOrdini;
+    public enum Stato {
+        ORDINATO(0),
+        INCORSO(1),
+        CONSEGNATO(2);
+
+        private int valore;
+
+        public int getValore() {
+            return valore;
+        }
+
+        Stato(int valore) {
+            this.valore = valore;
+        }
+
     }
-	
-    public Ordine(String numeroOrdini) {
-        this.numeroOrdini = numeroOrdini;
+
+	private long id;
+
+    private float importo;
+
+    private LocalDate data;
+
+    private String cliente;
+
+    private Stato stato;
+
+    public long getId() {
+        return id;
+    }
+
+    public float getImporto() {
+        return importo;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public Stato getStato() {
+        return stato;
+    }
+
+    public void setStato(Stato stato) {
+        this.stato = stato;
+    }
+
+    public Ordine(
+        long id,
+        float importo,
+        LocalDate data,
+        String cliente,
+        Stato stato
+    ) {
+        this.id = id;
+        this.importo = importo;
+        this.data = data;
+        this.cliente = cliente;
+        this.stato = stato;
     }
 
 }
